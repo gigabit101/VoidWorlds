cc37fc79c928f71a97c5d158918ed1b8dd686dd2
gigabit101 <thegigabit101@gmail.com>
2016-07-31 22:12:26 +0100

    clean imports and build 1.0.0

--- src/main/java/gigabit101/VoidWorlds/VoidEndGenerator.java
+++ src/main/java/gigabit101/VoidWorlds/VoidEndGenerator.java
@@ -21,44 +21,12 @@ public class VoidEndGenerator extends WorldProviderEnd
     public static class ChunkProviderEndVoid extends ChunkProviderEnd
     {
         private World world;
-//        private Random endRNG;
-//        private WorldGenSpikes spikes = new WorldGenSpikes();
-//        private MapGenEndCity endCityGen = new MapGenEndCity(this);
 
         public ChunkProviderEndVoid(World world, long seed)
         {
             super(world, false, seed);
             this.world = world;
-//            this.endRNG = new Random(seed);
         }
-
-//        @Override public void populate(int x, int z)
-//        {
-//            if (YUNoMakeGoodMap.instance.shouldBeVoid(world))
-//            {
-//                WorldGenSpikes.EndSpike[] aworldgenspikes$endspike = BiomeEndDecorator.getSpikesForWorld(world);
-//
-//                for (WorldGenSpikes.EndSpike worldgenspikes$endspike : aworldgenspikes$endspike)
-//                {
-//                    if (worldgenspikes$endspike.doesStartInChunk(new BlockPos(x*16, 0, z*16)))
-//                    {
-//                        this.spikes.setSpike(worldgenspikes$endspike);
-//                        this.spikes.generate(world, endRNG, new BlockPos(worldgenspikes$endspike.getCenterX(), 45, worldgenspikes$endspike.getCenterZ()));
-//                    }
-//                }
-//            }
-//
-//            if (YUNoMakeGoodMap.instance.shouldGenerateEndCities(this.world))
-//            {
-//                this.endCityGen.generateStructure(world, endRNG, new ChunkPos(x, z));
-//            }
-//
-//            if (x == 0 && z == 0)
-//            {
-//                // Allows exit portal to be placed correctly. DragonFightManager will take over from here...
-//                world.setBlockState(new BlockPos(0, 45, 0), Blocks.END_STONE.getDefaultState());
-//            }
-//        }
 
         @Override
         public Chunk provideChunk(int x, int z)
